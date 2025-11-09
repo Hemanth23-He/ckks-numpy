0b7f3a27bbb020ae05fbd04d178cdbef63fcd2cf
Hemanth23-He <goganaboinahemanth@gmail.com>
2025-11-10 02:31:40 +0530

    Convert poly to numpy array in Plaintext class

--- util/plaintext.py
+++ util/plaintext.py
@@ -1,30 +1,30 @@
 """A module to keep track of a plaintext."""
+import numpy as np
 
 class Plaintext:
-
     """An instance of a plaintext.
-
+    
     This is a wrapper class for a plaintext, which consists
     of one polynomial.
-
+    
     Attributes:
         poly (Polynomial): Plaintext polynomial.
         scaling_factor (float): Scaling factor.
     """
-
+    
     def __init__(self, poly, scaling_factor=None):
         """Sets plaintext to given polynomial.
-
+        
         Args:
             poly (Polynomial): Plaintext polynomial.
             scaling_factor (float): Scaling factor.
         """
-        self.poly = poly
+        self.poly = np.asarray(poly)
         self.scaling_factor = scaling_factor
-
+    
     def __str__(self):
         """Represents plaintext as a readable string.
-
+        
         Returns:
             A string which represents the Plaintext.
         """
