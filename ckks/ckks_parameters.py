9a5fa5b8d9da38f6a85bca9ec4652afee839a6f6
Hemanth23-He <goganaboinahemanth@gmail.com>
2025-11-10 04:20:23 +0530

    Update ckks_parameters.py

--- ckks/ckks_parameters.py
+++ ckks/ckks_parameters.py
@@ -1,12 +1,11 @@
 """A module to keep track of parameters for the CKKS scheme."""
-
 import math
+import numpy as np
 from util.crt import CRTContext
 
+
 class CKKSParameters:
-
     """An instance of parameters for the CKKS scheme.
-
     Attributes:
         poly_degree (int): Degree d of polynomial that determines the
             quotient ring R.
@@ -19,11 +18,9 @@ class CKKSParameters:
         prime_size (int): Minimum number of bits in primes for RNS representation.
         crt_context (CRTContext): Context to manage RNS representation.
     """
-
     def __init__(self, poly_degree, ciph_modulus, big_modulus, scaling_factor, taylor_iterations=6,
                  prime_size=59):
         """Inits Parameters with the given parameters.
-
         Args:
             poly_degree (int): Degree d of polynomial of ring R.
             ciph_modulus (int): Coefficient modulus of ciphertexts.
@@ -41,20 +38,18 @@ class CKKSParameters:
         self.num_taylor_iterations = taylor_iterations
         self.hamming_weight = poly_degree // 4
         self.crt_context = None
-
         if prime_size:
-            num_primes = 1 + int((1 + math.log(poly_degree, 2) + 4 * math.log(big_modulus, 2) \
-             / prime_size))
+            num_primes = 1 + int((1 + np.log2(poly_degree) + 4 * np.log2(big_modulus)) / prime_size)
             self.crt_context = CRTContext(num_primes, prime_size, poly_degree)
-
+    
     def print_parameters(self):
         """Prints parameters.
         """
         print("Encryption parameters")
         print("\t Polynomial degree: %d" %(self.poly_degree))
-        print("\t Ciphertext modulus size: %d bits" % (int(math.log(self.ciph_modulus, 2))))
-        print("\t Big ciphertext modulus size: %d bits" % (int(math.log(self.big_modulus, 2))))
-        print("\t Scaling factor size: %d bits" % (int(math.log(self.scaling_factor, 2))))
+        print("\t Ciphertext modulus size: %d bits" % (int(np.log2(self.ciph_modulus))))
+        print("\t Big ciphertext modulus size: %d bits" % (int(np.log2(self.big_modulus))))
+        print("\t Scaling factor size: %d bits" % (int(np.log2(self.scaling_factor))))
         print("\t Number of Taylor iterations: %d" % (self.num_taylor_iterations))
         if self.crt_context:
             rns = "Yes"
